3469cc9fb65915e76bae4bef2eb39e7ce343897f
man85 <man85@yandex.ru>
2021-05-01 19:32:35 +0300

    6-DI-setter

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -2,9 +2,15 @@ package org.example;
 
 public class MusicPlayer {
     private Music music;
+
     public MusicPlayer(Music music){
       this.music=music;
     }
+    public MusicPlayer(){}
+    public void setMusic(Music music){
+        this.music=music;
+    }
+
     public void playMusic(){
         System.out.println("Playing "+this.music.getSong());
     }
