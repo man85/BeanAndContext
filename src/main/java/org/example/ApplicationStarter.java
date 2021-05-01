08d6d54cc513c349ddf6561f5e8c7f9f89bb8da5
man85 <man85@yandex.ru>
2021-05-01 19:08:47 +0300

    5-DI

--- src/main/java/org/example/ApplicationStarter.java
+++ src/main/java/org/example/ApplicationStarter.java
@@ -5,8 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationStarter {
     public static void main(String[] args){
         ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music=context.getBean("BeanMusic",Music.class);
-        MusicPlayer musicPlayer=new MusicPlayer(music);
+        //Music music=context.getBean("BeanMusic",Music.class);
+        //MusicPlayer musicPlayer=new MusicPlayer(music);
+        MusicPlayer musicPlayer=context.getBean("musicPlayer",MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
 
